f1c72641a444ad1162c8e9b048d31eaf8f6edc8a
Chumy <57281249+Jimmy01240397@users.noreply.github.com>
2023-07-10 20:50:30 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/backend',methods=['GET']) # 瀏覽網頁路徑 /backend 時要做甚麼
 def test():
-    ans = int(request.args['a']) + int(request.args['b'])  # 參數 a 加上 參數 b，將結果存入 ans
+    ans = int(flask.request.args['a']) + int(flask.request.args['b'])  # 參數 a 加上 參數 b，將結果存入 ans
     with open('/tmp/nowtest.txt', 'w') as f:
         f.write(str(ans))  # 將 ans 內容輸出到 /tmp/nowtest.txt 檔案內
     return str(ans) # 回傳 ans 內容
